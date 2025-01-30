673de5e7591b422dfe27bd5a3d444e98177f5003
Rahul <rahulpratik1212@gmail.com>
2025-01-31 00:38:52 +0530

    add temperature

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const temperature = document.getElementById('temperature');
+const city = document.getElementById('city');
+
 async function getWeatherData(place) {
   const url =
     "https://api.weatherstack.com/current?access_key=c4bd84c2fa4b9bb3aa9fd5964f7ea2f4&query=place";
@@ -8,10 +11,12 @@ async function getWeatherData(place) {
   try {
     const response = await fetch(url, options);
     const result = await response.text();
-    console.log(result);
+    const data = JSON.parse(result);
+    temperature.textContent = `${data.current.temperature}°C`;
+    console.log(data);
   } catch (error) {
     console.error(error);
   }
 }
 
-// getWeatherData("America");
+getWeatherData("America");
