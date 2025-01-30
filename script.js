2a3d04b2285660e9aa09017bbb266bc584f23080
Rahul <rahulpratik1212@gmail.com>
2025-01-31 01:18:14 +0530

    add temp

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const temperature = document.getElementById('temperature');
-const city = document.getElementById('city');
+const temperature = document.getElementById("temperature");
+const cityName = document.getElementById("city-name");
+const day = document.getElementById("day");
+const time = document.getElementById("time");
+// const date = document.getElementById("date");
 
 async function getWeatherData(place) {
-  const url =
-    "https://api.weatherstack.com/current?access_key=c4bd84c2fa4b9bb3aa9fd5964f7ea2f4&query=place";
+  const presentDay = getDay(new Date());
+  console.log(presentDay);
+
+  const url = `https://api.weatherstack.com/current?access_key=c4bd84c2fa4b9bb3aa9fd5964f7ea2f4&query=${place}`;
   const options = {
     method: "GET",
   };
@@ -13,6 +18,9 @@ async function getWeatherData(place) {
     const result = await response.text();
     const data = JSON.parse(result);
     temperature.textContent = `${data.current.temperature}°C`;
+    cityName.textContent = data.location.name;
+    day.textContent = presentDay;
+    time.textContent = data.current.observation_time;
     console.log(data);
   } catch (error) {
     console.error(error);
@@ -20,3 +28,21 @@ async function getWeatherData(place) {
 }
 
 getWeatherData("America");
+
+function getDay(date) {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = days[date.getDay()];
+  const dayNum = date.getDate();
+  const month = date.toLocaleString("en-US", { month: "short" });
+  const year = date.getFullYear().toString().slice(-2);
+
+  return {day, dayNum, month, year}
+}
